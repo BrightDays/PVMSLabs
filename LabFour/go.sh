3140e67122bd8ff5d238f22e00340342882a14de
BrightDays <brightdayswork@gmail.com>
2015-10-12 16:32:56 +0300

    removed useless script

--- LabFour/go.sh
+++ LabFour/go.sh
@@ -6,45 +6,59 @@ sudo sh load.sh
 echo "#1 n = 1"\\r
 echo 1 > /dev/calc_number
 result=$(cat /dev/calc_result)
-echo "fib(1) == $result"\\r
-sudo sh unload.sh
+if [ "$result" -eq "1" ];
+then
+    echo "OK. fib(1) == $result"\\r
+else
+    echo "ERROR. fib(1) != $result"\\r
+fi
 
-sudo sh load.sh
+
 echo "#2 n = 2"\\r
 echo 2 > /dev/calc_number
 result=$(cat /dev/calc_result)
-echo "fib(2) == $result"\\r
-sudo sh unload.sh
+if [ "$result" -eq "1" ];
+then
+    echo "OK. fib(2) == $result"\\r
+else
+    echo "ERROR. fib(2) != $result"\\r
+fi
 
 
-sudo sh load.sh
-echo "#3 n = 13"\\r
-echo 13 > /dev/calc_number
+echo "#3 n = 3"\\r
+echo 3 > /dev/calc_number
 result=$(cat /dev/calc_result)
-echo "fib(13) == $result"\\r
-sudo sh unload.sh
+if [ "$result" -eq "2" ];
+then
+    echo "OK. fib(3) == $result"\\r
+else
+    echo "ERROR. fib(3) != $result"\\r
+fi
 
 
-sudo sh load.sh
-echo "#5 n = 24"\\r
-echo 24 > /dev/calc_number
+
+echo "#4 n = 10"\\r
+echo 10 > /dev/calc_number
 result=$(cat /dev/calc_result)
-echo "fib(24) == $result"\\r
-sudo sh unload.sh
+if [ "$result" -eq "55" ];
+then
+    echo "OK. fib(10) == $result"\\r
+else
+    echo "ERROR. fib(10) != $result"\\r
+fi
 
 
-sudo sh load.sh
-echo "#4 n = -12"\\r
-echo -12 > /dev/calc_number
+echo "#5 n = -13"\\r
+echo -13 > /dev/calc_number
 result=$(cat /dev/calc_result)
-echo "fib(-12) == $result"\\r
-sudo sh unload.sh
+echo "fib(-13) == $result"\\r
+if [ "$result" -eq "-233" ];
+then
+    echo "OK. fib(-13) == $result"\\r
+else
+    echo "ERROR. fib(-13) != $result"\\r
+fi
 
 
-sudo sh load.sh
-echo "#5 n = 24"\\r
-echo 24 > /dev/calc_number
-result=$(cat /dev/calc_result)
-echo "fib(24) == $result"\\r
 sudo sh unload.sh
 
