2c0c09ea76b6d82346d821819b849ade22702dd0
BrightDays <brightdayswork@gmail.com>
2015-11-30 19:18:51 +0300

    lab3, lab4

---
+++ android/Lab3/MyShows/app/src/main/java/com/example/brightdays/myshows/NewEpisodes.java
@@ -0,0 +1,14 @@
+package com.example.brightdays.myshows;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class NewEpisodes extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_episodes);
+    }
+
+}
